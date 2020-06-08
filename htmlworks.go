0d674e55b09276488832e44e98fc1a5f19abb9fe
Kazuki Minakawa <k.minakawa@playree.com>
2020-06-08 22:59:49 +0900

    静的HTML生成機能

--- htmlworks.go
+++ htmlworks.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"html/template"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"strings"
 	"time"
@@ -21,6 +24,7 @@ type DirectoriesConfig struct {
 	Contents  string `toml:"contents"`
 	Exclusion string `toml:"exclusion"`
 	Resources string `toml:"resources"`
+	Generate  string `toml:"generate"`
 }
 
 // ServerConfig various server settings.
@@ -51,7 +55,7 @@ func main() {
 
 	// Load config file.
 	if _, err := toml.DecodeFile(configFilePath, &conf); err != nil {
-		log.Fatal(err)
+		log.Fatalln("Error:", err)
 	}
 
 	switch mode {
@@ -87,7 +91,7 @@ func procServer() {
 		http.StripPrefix(
 			"/"+conf.Directories.Resources+"/",
 			http.FileServer(http.Dir(conf.Directories.Resources+"/"))))
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", conf.Server.Port), nil))
+	log.Fatalln(http.ListenAndServe(fmt.Sprintf(":%d", conf.Server.Port), nil))
 }
 
 func handleServer(w http.ResponseWriter, r *http.Request) {
@@ -95,11 +99,21 @@ func handleServer(w http.ResponseWriter, r *http.Request) {
 	log.Println("RequestURI:", r.RequestURI)
 	filename := convFilename(r.RequestURI)
 
-	buf, err := ioutil.ReadFile(goingtpl.GetBaseDir() + filename)
+	err := executeWriter(w, filename)
 	if err != nil {
 		log.Println("Not found file:", filename)
 		http.NotFound(w, r)
 		return
+	}
+
+	log.Printf("ExecTime=%d MicroSec\n",
+		(time.Now().UnixNano()-start)/int64(time.Microsecond))
+}
+
+func executeWriter(w io.Writer, filename string) error {
+	buf, err := ioutil.ReadFile(filepath.Join(conf.Directories.Contents, filename))
+	if err != nil {
+		return err
 	}
 	log.Println("Load file:", filename)
 
@@ -119,9 +133,7 @@ func handleServer(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
-
-	log.Printf("ExecTime=%d MicroSec\n",
-		(time.Now().UnixNano()-start)/int64(time.Microsecond))
+	return nil
 }
 
 func extParam(contents string) (map[string]interface{}, string, error) {
@@ -140,16 +152,60 @@ func extParam(contents string) (map[string]interface{}, string, error) {
 }
 
 func procGenerate() {
+	start := time.Now().UnixNano()
 	log.Println("Start Generate ========")
 	log.Println("Contents directory:", conf.Directories.Contents)
 	log.Println("Exclusion directory:", conf.Directories.Exclusion)
 	log.Println("Resources directory:", conf.Directories.Resources)
 
+	goingtpl.SetBaseDir(conf.Directories.Contents)
+
 	targetFileList := getTarget(conf.Directories.Contents)
 	if len(targetFileList) > 0 {
 		log.Println("Target file:", len(targetFileList))
-		log.Println(targetFileList)
+		for _, file := range targetFileList {
+			// 対象を生成
+			var buffSrc bytes.Buffer
+			executeWriter(&buffSrc, file)
+
+			targetPath := filepath.Join(conf.Directories.Generate, file)
+			targetDir := filepath.Dir(targetPath)
+
+			// ディレクトリの確認と作成
+			if _, err := os.Stat(targetDir); os.IsNotExist(err) {
+				err := os.MkdirAll(targetDir, 0777)
+				if err != nil {
+					log.Fatalln("Error:", err)
+				}
+			}
+
+			// 生成先の確認
+			buffDest, err := ioutil.ReadFile(targetPath)
+			if err != nil {
+				// ファイルが無ければ生成
+				log.Println("Create:", targetPath)
+				err := ioutil.WriteFile(targetPath, buffSrc.Bytes(), 0664)
+				if err != nil {
+					log.Fatalln("Error:", err)
+				}
+			} else {
+				// ファイルが在れば、差分を比較して更新
+				if bytes.Compare(buffSrc.Bytes(), buffDest) != 0 {
+					log.Println("Update:", targetPath)
+					err := ioutil.WriteFile(targetPath, buffSrc.Bytes(), 0664)
+					if err != nil {
+						log.Fatalln("Error:", err)
+					}
+				} else {
+					log.Println("Pass:", targetPath)
+				}
+			}
+		}
+	} else {
+		log.Println("Target file not found.")
 	}
+	log.Printf("ExecTime=%d MicroSec\n",
+		(time.Now().UnixNano()-start)/int64(time.Microsecond))
 }
 
 func convFilename(uri string) string {
